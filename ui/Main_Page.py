f35d4204efad659f0d398d859117b5c9b10b2ccc
Guillaume Gavillet <guillaumegav@gmail.com>
2024-06-05 16:22:30 +0200

    app almost done

--- ui/Main_Page.py
+++ ui/Main_Page.py
@@ -23,6 +23,7 @@ def highlight_age(value):
         return 'color: red'
 # st.markdown("""# Project Awesome: Overview
 # List your companies by applying the necessary filters.""")
+# Function to clear search query
 
 # pull the csv file
 file_path = os.path.abspath(os.path.dirname(__file__))
@@ -34,29 +35,8 @@ df = pd.DataFrame(data)
 
 # set ticker as index
 df = df.set_index('Ticker')
-
-# # create a multiselect filter for the 'Classification' column
-# selected_classifications = st.multiselect('Select 2024 Ratio Stock/S&P500', df['Recommendation'].unique())
-
-# # filter the df based on the selected classifications
-# if selected_classifications:
-#     filtered_df = df[df['Recommendation'].isin(selected_classifications)]
-# else:
-#     filtered_df = df
-
-# # create a multiselect filter for the 'Industry' column
-# selected_industries = st.multiselect('Select Sectors', filtered_df['Sector'].unique())
-
-# # filter the df based on the selected classifications
-# if selected_industries:
-#     filtered_df_1 = filtered_df[filtered_df['Sector'].isin(selected_industries)]
-# else:
-#     filtered_df_1 = filtered_df
-
-
-
-
 # Set CSS styling
+
 css_path = os.path.join('style.css')
 with open(css_path) as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
@@ -64,44 +44,60 @@ with open(css_path) as f:
 # Set page title and description
 st.markdown(
     """
-    <h1 class="title">Stock Market Investor</h1>
-    <p class="subtitle">List your companies by applying the necessary filters.</p>
+    <h1 class="title">Stock Market Advisor</h1>
+    <p class="subtitle">Companies List</p>
     """,
     unsafe_allow_html=True
 )
-
-# # Load data
-# data = pd.read_csv('../raw_data/raw_data_ui.csv')
-# df = pd.DataFrame(data)
-
-# # Set ticker as index
-# df = df.set_index('Ticker')
-
+# Apply filters
+filtered_df = df
 # Sidebar filters
 with st.sidebar:
     st.title('Filters')
     selected_classifications = st.multiselect('Select 2024 Ratio Stock/S&P500', df['Recommendation'].unique())
     selected_industries = st.multiselect('Select Sectors', df['Sector'].unique())
+    # search bar
+    search_query = st.text_input('Enter search query:')
+    line_count = st.number_input("Number of rows to display", 1, 1000, 3)
+    # Select sort column
+    sort_by = st.selectbox('Sort by', options=['None'] + list(filtered_df.columns), index=0)
+    sort_descending = st.checkbox('Sort descending', value=False)
 
-# Apply filters
-filtered_df = df
+
+
+
 if selected_classifications:
     filtered_df = filtered_df[filtered_df['Recommendation'].isin(selected_classifications)]
+    search_query=''
+
 if selected_industries:
     filtered_df = filtered_df[filtered_df['Sector'].isin(selected_industries)]
+    search_query = ''
+
+if search_query:
+    filtered_df = df[df.apply(lambda row: search_query.lower() in row.astype(str).str.lower().to_string(), axis=1)]
+
+
+
+if sort_by != 'None':
+    if pd.api.types.is_numeric_dtype(filtered_df[sort_by]):
+        filtered_df = filtered_df.sort_values(by=sort_by, ascending=not sort_descending)
+    else:
+        filtered_df = filtered_df.sort_values(by=sort_by, ascending=not sort_descending)
+
 
 # Display filtered dataframe
-line_count = st.number_input("Number of rows to display", 1, 1000, 10)
 head_df = filtered_df.head(line_count)
 styled_df = apply_styles(head_df)
 st.write(styled_df)
 
 # Display line chart
-st.title('Stock Ratio Over Years')
-with st.echo():
-    df_drop = head_df.drop(columns=['Name', 'Last Sale', 'Market Cap', 'Sector', 'Industry', 'Recommendation'])
-    df_drop_transposed = df_drop.T
-    st.line_chart(df_drop_transposed)
+st.subheader('Stock Ratio Over Years')
+
+df_drop = head_df.drop(columns=['Name', 'Last Sale', 'Market Cap', 'Sector', 'Industry', 'Recommendation'])
+df_drop_transposed = df_drop.T
+st.line_chart(df_drop_transposed)
+
 
 # Display Google stock price prediction button
 if st.button('Display Google stock price prediction'):
